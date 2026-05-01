79ec08527bf6f0eaabac559088700319e47362ec
Isam David <isamdavid17@gmail.com>
2026-05-01 17:33:45 -0600

    Add Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "@/app/globals.css";
 
@@ -35,6 +36,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <PwaSetup />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
